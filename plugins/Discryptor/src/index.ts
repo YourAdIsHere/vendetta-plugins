2b70a4fbdfcbd04255a0c2ee0bd79385206a7c74
YourAdIsHere <63565008+YourAdIsHere@users.noreply.github.com>
2024-07-10 07:48:42 -0700

    it works

--- plugins/Discryptor/src/index.ts
+++ plugins/Discryptor/src/index.ts
@@ -43,7 +43,7 @@ const handleMessage = (msg: any) => {
         if (msg.content && isEncrypted(msg.content)) {
             msg.content = decryptContent(msg.content);
         } else {
-            msg.content = encryptContent(msg.content);
+           // msg.content = encryptContent(msg.content);
         }
     }
 };
