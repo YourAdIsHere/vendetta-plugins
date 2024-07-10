d5473bb3007eb14c20bd9a30d7b964648b28ee27
YourAdIsHere <63565008+YourAdIsHere@users.noreply.github.com>
2024-07-10 10:20:09 -0700

    enter

--- plugins/Discryptor/src/index.ts
+++ plugins/Discryptor/src/index.ts
@@ -72,7 +72,7 @@ const handleContent = (content: Content[]) => {
 const processRows = (rows: any[]) => {
     for (const row of rows) {
         if (row.message?.content) {
-            handleMessage(row.message);
+        row.message = handleMessage(row.message);
         }
     }
     return rows;
